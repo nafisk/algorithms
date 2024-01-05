8c74a7e8169b4d3d0e901191ed8e129b1335f0ca
nafisk <nafisrizwank@gmail.com>
2024-01-05 12:46:53 -0500

    add bottom up approach

--- dp/fibMemo.py
+++ dp/fibMemo.py
@@ -1,3 +1,6 @@
+'''
+    💡 Top Down Approach
+'''
 # fib without memo
 def fib(i):
     if i <= 0: return 0
@@ -15,3 +18,39 @@ def fibMemo(val):
         return memo[i]
     
     return helper(100)
+
+
+
+'''
+    💡 Bottom Up Approach
+'''
+
+# with array
+def fibBotUp(val):
+    if val <= 2: return 1
+    
+    arr = [0] * val
+    arr[1] = 1
+    
+    for i in range(2, val):
+        arr[i] = arr[i - 1] + arr[i - 2]
+    return arr[-1] + arr[-2]
+
+
+# without array
+def fibBotUp_(val):
+    if val <= 0: return 0
+    a = 0
+    b = 1
+    
+    for i in range(2, val):
+        c = a + b 
+        a = b
+        b = c
+    return a + b
+
+if __name__ == '__main__':
+    # print(fib(500))
+    # print(fibMemo(500))
+    # print(fibBotUp(16))
+    # print(fibBotUp_(16))
